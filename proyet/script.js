5eaf7d2197155884fc88c5a52dd405fc377af0f3
AngelJohank <74012324+AngelJohank@users.noreply.github.com>
2021-11-03 12:21:48 -0500

    Delete script.js

--- proyet/script.js
+++
@@ -1,7 +0,0 @@
-// class Animal(nombre, edad, tamaño, raza)
-
-let animales = {
-    perrito: new Animal("Pepe", 6, 200, "chiwawa"),
-    gato: new Animal("Maluma", 3, 100, "blanco / negro"),
-    lorito: new Animal("Perla", 3, 100, "verde con cabeza azul"),
-}
